bd0bc64d9d9eb3c9bcc670cbb418daa7b90c46f7
birugh <notblue.https@gmail.com>
2025-09-16 07:30:46 +0700

    update: siswa migration

--- database/migrations/2025_09_15_101913_create_siswas_table.php
+++ database/migrations/2025_09_15_101913_create_siswas_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('nis', 100)->unique  ();
             $table->string('nama_siswa', 50);
             $table->integer('id_kelas');
-            $table->string('password', 50);
+            $table->string('password', 100);
             $table->timestamps();
         });
     }
