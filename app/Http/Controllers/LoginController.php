b151f0bf3958b9d8f2f6d6e0fe0797590e0768b2
birugh <notblue.https@gmail.com>
2025-09-16 08:47:14 +0700

    update: menambahkan logic login walas dan login siswa

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -16,4 +16,48 @@ class LoginController extends Controller
     {
         return view('login');
     }
+
+    public function loginWalas(Request $request)
+    {
+        $walas = Walas::where('nig', $request->txt_nig)->first();
+
+        if (! $walas || ! Hash::check($request->txt_pass, $walas->password)) {
+            return redirect()->back()->with('error', 'NIG atau Password salah');
+        }
+
+        $kelas = Kelas::where('id', $walas->kelas_id)->first();
+
+        session([
+            'role' => 'Walas',
+            'id' => $walas->id,
+            'nig' => $walas->nig,
+            'nama' => $walas->nama_walas,
+            'kelas_id' => $walas->kelas_id,
+            'nama_kelas' => $kelas ? $kelas->nama_kelas : 'Kelas Belum ditentukan',
+        ]);
+
+        return redirect('nilai-raport/index');
+    }
+
+    public function loginSiswa(Request $request)
+    {
+        $siswa = Siswa::where('nis', $request->txt_nis)->first();
+
+        if (! $siswa || ! Hash::check($request->txt_pass, $siswa->password)) {
+            return redirect()->back()->with('error', 'NIS atau Password salah');
+        }
+
+        $kelas = Kelas::where('id', $siswa->kelas_id)->first();
+
+        session([
+            'role' => 'Murid',
+            'id' => $siswa->id,
+            'nis' => $siswa->nis,
+            'nama' => $siswa->nama_siswa,
+            'kelas_id' => $siswa->kelas_id,
+            'nama_kelas' => $kelas ? $kelas->nama_kelas : 'Kelas Belum ditentukan',
+        ]);
+
+        return redirect('nilai-raport/show');
+    }
 }
