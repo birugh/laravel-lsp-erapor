e0e3dbae7646c3f2bf57fca30b0a1286d768bf1f
birugh <notblue.https@gmail.com>
2025-09-16 08:39:39 +0700

    add: menambahkan route login dan nilai controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\NilaiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/', 'index');
+    Route::post('/login-walas', 'loginWalas');
+    Route::post('/login-siswa', 'loginSiswa');
+    Route::get('/dashboard', 'dashboard');
+    Route::get('/logout', 'logout');
+});
+
+
+Route::middleware('CheckUserRole:Walas')->group(function () {
+    Route::controller(NilaiController::class)
+        ->prefix('nilai-raport')
+        ->group(function () {
+            Route::get('/index', 'index');
+            Route::get('/create', 'create');
+            Route::post('/store', 'store');
+            Route::get('/edit/{nilai}', 'edit');
+            Route::put('/update/{nilai}', 'update');
+            Route::get('/destroy/{nilai}', 'destroy');
+            Route::get('/show/{id}', 'showNilai');
+            Route::get('/show', 'show');
+        });
 });
