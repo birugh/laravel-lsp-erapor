a64faa00c13bc37d5f738a89e1f8e3aee4cf0c32
birugh <notblue.https@gmail.com>
2025-09-22 16:55:11 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -14,27 +14,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::controller(LoginController::class)->group(function () {
-    Route::get('/', 'index');
-    Route::post('/login-walas', 'loginWalas');
-    Route::post('/login-siswa', 'loginSiswa');
-    Route::get('/dashboard', 'dashboard');
-    Route::get('/logout', 'logout');
-});
-
-Route::middleware('CheckUserRole:Walas')->group(function () {
-    Route::controller(NilaiController::class)
-        ->prefix('nilai-raport')
-        ->group(function () {
-            Route::get('/index', 'index');
-            Route::get('/create', 'create');
-            Route::post('/store', 'store');
-            Route::get('/edit/{nilai}', 'edit');
-            Route::put('/update/{nilai}', 'update');
-            Route::get('/destroy/{nilai}', 'destroy');
-            Route::get('/show/{id}', 'showNilai');
-            Route::get('/show', 'showForStudent');
-        });
-});
-
